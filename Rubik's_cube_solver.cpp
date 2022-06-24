ba365025405a51b38fc5b50bcd918035ea402e57
Saipriya-1144 <87598867+Saipriya-1144@users.noreply.github.com>
2022-06-24 19:59:25 +0530

    Update Rubik's_cube_solver.cpp

--- Rubik's_cube_solver.cpp
+++ Rubik's_cube_solver.cpp
@@ -3,20 +3,22 @@ using namespace std;
 
 
 /*
-0->top face
-1->front face
-2->left face
-3->down face
-4->back face
-5->right face
+Store Cube in vector of vector
+
+0 index for top face
+1 index for front face
+2 index for left face
+3 index for down face
+4 index for back face
+5 index for right face
 */
 
 class cube{
     public:
      vector<vector<int>> rubics_cube;
 
-     void take_input();
-     void print_cube();
+     void take_input();//takes input into 2d vector
+     void print_cube();//prints the cube
      
      void right_face_cw(int r);//rotate right face x times in clock wise direction
      void right_face_acw(int r);//rotate right face x times in Anticlock wise direction
@@ -36,32 +38,32 @@ class cube{
      void face_2layers_cw(int r);//rotate front face 2 layers x times in clock wise direction
      void face_2layers_acw(int r);//rotate front face 2 layers x times in Anticlock wise direction
 
-     void locate_origin();
+     void bottom_face_plus();//Solving edges in last layer
 
      void righty_alg(int r);
      void lefty_alg(int r);
      void to_fix_with_righty_alg();
-     void bottom_layer_corners();
+     void bottom_layer_corners();//solving corners in the last layer
 
      void to_move_edge_right();
      void to_move_edge_left();
-     void second_layer_edges(); 
+     void second_layer_edges(); //solving 2nd layer edges
 
      int check_form_in_step4();
-     void top_layer_plus_step4();
+     void top_layer_plus_step4();//Making plus on top face
 
      int check_form_in_step5();
      void sqnce_in_step5();
-     void position_toplayer_corners();
+     void position_toplayer_corners();//positioning the top layer corners(corner should be between correct centers)
 
-     void twisting_top_layer_corners();
+     void twisting_top_layer_corners(); //solving top layer corners (twisting them after they are correctly positioned)
 
      void sqnce_for_top_layer_edges();
      int check_form_in_lst_step();
-     void top_layer_edges();
+     void top_layer_edges(); //solving top layer edges
 };
 
-void cube::print_cube(){
+void cube::print_cube(){ //to print the cube
       cout << "\nTop face of cube:\n";
    for(int i=1;i<=9;i++){
       char x=rubics_cube[0][i]+'a';
@@ -101,7 +103,7 @@ void cube::print_cube(){
    cout << "\n";
 }
 
-void cube::take_input(){
+void cube::take_input(){ //to take the input
     rubics_cube.resize(6);
     for(int i=0;i<6;i++){
         rubics_cube[i].resize(10);
@@ -146,16 +148,12 @@ void cube::take_input(){
    }
 }
 
-void cube::right_face_cw(int r){
+void cube::right_face_cw(int r){// to rotate right face r times in clock wise direction
 
    if(r==3){
      //write acw once
      right_face_acw(1);
      return;
-   }
-
-   else{
-      cout << "Rotate right face " << r << " times in clockwise direction\n";
    }
 
 while(r--){
@@ -192,12 +190,10 @@ while(r--){
 }
 }
 
-void cube:: right_face_acw(int r){
+void cube:: right_face_acw(int r){//To rotate right face r times in Anticlock wise direction
    if(r==3){
       right_face_cw(1);
       return;
-   }else{
-      cout << "Rotate right face " << r << " times in Anticlockwise direction\n";
    }
    
  while(r--){
@@ -238,12 +234,10 @@ void cube:: right_face_acw(int r){
 
 }
 
-void cube::left_face_cw(int r){
+void cube::left_face_cw(int r){//To rotate left face r times in clock wise direction
    if(r==3){
       left_face_acw(1);
       return;
-   }else{
-      cout << "Rotate left face " << r << " times in clockwise direction\n";
    }
 
  while(r--){ 
@@ -283,12 +277,10 @@ void cube::left_face_cw(int r){
 }  
 }
 
-void cube::left_face_acw(int r){
+void cube::left_face_acw(int r){//To rotate left face r times in Anticlock wise direction
    if(r==3){
       left_face_cw(1);
       return;
-   }else{
-      cout << "Rotate left face " << r << " times in Anticlockwise direction\n";
    }
 
  while(r--){
@@ -327,12 +319,10 @@ void cube::left_face_acw(int r){
 
 }
 
-void cube::top_face_cw(int r){
+void cube::top_face_cw(int r){//To rotate top face r times in clock wise direction
   if(r==3){
      top_face_acw(1);
      return;
-  }else{
-     cout << "Rotate top face " << r << " times in clockwise direction\n";
   }
 
   while(r--){
@@ -373,12 +363,10 @@ void cube::top_face_cw(int r){
 }
 }
 
-void cube::top_face_acw(int r){
+void cube::top_face_acw(int r){//To rotate top face r times in Anticlock wise direction
    if(r==3){
      top_face_cw(1);
      return;
-  }else{
-     cout << "Rotate top face " << r << " times in Anticlockwise direction\n";
   }
 
  while(r--){
@@ -418,12 +406,10 @@ void cube::top_face_acw(int r){
 }
 }
 
-void cube::bottom_face_cw(int r){
+void cube::bottom_face_cw(int r){//To rotate bottom face r times in clock wise direction
   if(r==3){
      bottom_face_acw(1);
      return;
-  }else{
-     cout << "Rotate bottom face " << r << " times in clockwise direction\n";
   }
 
 while(r--){
@@ -463,12 +449,10 @@ while(r--){
 }
 }
 
-void cube::bottom_face_acw(int r){
+void cube::bottom_face_acw(int r){//To rotate bottom face r times in Anticlock wise direction
   if(r==3){
      bottom_face_cw(1);
      return;
-  }else{
-     cout << "Rotate bottom face " << r << " times in Anticlockwise direction\n";
   }
 
  while(r--){
@@ -508,12 +492,10 @@ void cube::bottom_face_acw(int r){
 }
 }
 
-void cube::front_face_cw(int r){
+void cube::front_face_cw(int r){//To rotate front face r times in clock wise direction
  if(r==3){
     front_face_acw(1);
     return;
- }else{
-    cout << "Rotate front face " << r << " times in clockwise direction\n";
  }
 
  while(r--){
@@ -573,12 +555,10 @@ void cube::front_face_cw(int r){
  }
 }
 
-void cube::front_face_acw(int r){
+void cube::front_face_acw(int r){//To rotate front face r times in Anticlock wise direction
   if(r==3){
     front_face_cw(1);
     return;
- }else{
-    cout << "Rotate front face " << r << " times in Anticlockwise direction\n";
  }
 
  while(r--){
@@ -638,12 +618,10 @@ void cube::front_face_acw(int r){
 }
 }
 
-void cube::bottom_2layers_cw(int r){
+void cube::bottom_2layers_cw(int r){//To rotate bottom 2 layers r times in clock wise direction
    if(r==3){
       bottom_2layers_acw(1);
       return;
-   }else{
-      cout << "Rotate bottom 2 layers " << r << " times in clockwise direction\n";
    }
 
    while(r--){
@@ -686,12 +664,10 @@ void cube::bottom_2layers_cw(int r){
  }
 }
 
-void cube::bottom_2layers_acw(int r){
+void cube::bottom_2layers_acw(int r){//To rotate bottom 2 layers r times in Anticlock wise direction
    if(r==3){
       bottom_2layers_cw(1);
       return;
-   }else{
-      cout << "Rotate bottom 2 layers " << r << " times in Anticlockwise direction\n";
    }
 
    while(r--){
@@ -736,7 +712,7 @@ void cube::bottom_2layers_acw(int r){
  }
 }
 
-void cube::spin_cw(int r){
+void cube::spin_cw(int r){//To spin cube r times in clock wise direction
    if(r==3){
       spin_acw(1);
       return;
@@ -746,7 +722,7 @@ void cube::spin_cw(int r){
    top_face_cw(r);
 }
 
-void cube::spin_acw(int r){
+void cube::spin_acw(int r){//To spin cube r times in Anticlock wise direction
    if(r==3){
       spin_cw(1);
       return;
@@ -756,12 +732,10 @@ void cube::spin_acw(int r){
    top_face_acw(r);
 }
 
-void cube::face_2layers_cw(int r){
+void cube::face_2layers_cw(int r){//To rotate face 2 layers r times in clock wise direction
    if(r==3){
       face_2layers_acw(1);
       return;
-   }else{
-      cout << "Rotate face 2 layers " << r << " times in clockwise direction\n";
    }
 
    while(r--){
@@ -833,12 +807,10 @@ void cube::face_2layers_cw(int r){
    }
 }
 
-void cube::face_2layers_acw(int r){
+void cube::face_2layers_acw(int r){//To rotate face 2 layers r times in Anticlock wise direction
   if(r==3){
       face_2layers_cw(1);
       return;
-   }else{
-      cout << "Rotate face 2 layers " << r << " times in Anticlockwise direction\n";
    }
 
    while(r--){
@@ -912,7 +884,7 @@ void cube::face_2layers_acw(int r){
 
 
 
-void cube::rotate_upside_down(){
+void cube::rotate_upside_down(){ //to rotate cube upside down
    swap(rubics_cube[0],rubics_cube[3]);
   reverse(rubics_cube[1].begin()+1,rubics_cube[1].end());
   reverse(rubics_cube[4].begin()+1,rubics_cube[4].end());
@@ -939,27 +911,9 @@ void cube::lefty_alg(int r){
    }
 }
 
-void cube::locate_origin(){//take white to top position
+void cube::bottom_face_plus(){//To solve bottom face edges
 
    int i;
-
-   //for taking white edges in bottom face to top
-   for(i=0;i<=4;i++){
-      if(rubics_cube[3][6]!=rubics_cube[3][5]){
-         bottom_face_cw(1);
-      }else{
-         int j;
-         for(j=0;j<4;j++){
-            if(rubics_cube[0][6]!=rubics_cube[3][5]){
-              right_face_cw(2);
-              break;
-            }else{
-              top_face_cw(1);
-            }
-         }
-      }
-   }
-   
 
   for(int k=0;k<=4;k++){
    for(int i=0;i<=4;i++){
@@ -983,6 +937,7 @@ void cube::locate_origin(){//take white to top position
          for(int j=0;j<4;j++){
             if(rubics_cube[0][6]!=rubics_cube[3][5]){
               right_face_cw(1);
+              break;
             }else{
               top_face_cw(1);
             }
@@ -991,9 +946,35 @@ void cube::locate_origin(){//take white to top position
    }
 
     spin_cw(1);
+
   }
 
-  
+   //for taking white edges in bottom face to top
+   for(i=0;i<=4;i++){
+      if(rubics_cube[3][6]!=rubics_cube[3][5]){
+         bottom_face_cw(1);
+      }else{
+         int j;
+         for(j=0;j<4;j++){
+            if(rubics_cube[0][6]!=rubics_cube[3][5]){
+              right_face_cw(2);
+              break;
+            }else{
+              top_face_cw(1);
+            }
+         }
+      }
+   }
+   
+   
+   for(int i=0;i<4;i++){
+   while(rubics_cube[1][2]!=rubics_cube[1][5]){
+      bottom_2layers_cw(1);
+   }
+
+   front_face_cw(2);
+   spin_cw(1);
+  }
    
 }
 
@@ -1003,7 +984,7 @@ void cube::to_fix_with_righty_alg(){
    }
 }
 
-void cube::bottom_layer_corners(){
+void cube::bottom_layer_corners(){//to solve bottom layer corners
    int t=8;
    while(t--){
       //checking if top right corner has white
@@ -1059,6 +1040,41 @@ void cube::bottom_layer_corners(){
 
    //verifying if bottom layer corners are properly placed
 
+   //to solve pieces in bottom layer that are correctly positioned but twisted
+   for(int i=0;i<4;i++){
+       
+       if(rubics_cube[1][9]==rubics_cube[1][5] && rubics_cube[5][7]==rubics_cube[5][5] && rubics_cube[3][3]==rubics_cube[3][5]){
+           spin_cw(1);
+           continue;
+       }
+       
+       int x,y;
+       
+       if(rubics_cube[1][9]==rubics_cube[3][5]){
+           x=rubics_cube[5][7];
+           y=rubics_cube[3][3];
+       }
+       
+       if(rubics_cube[5][7]==rubics_cube[3][5]){
+           x=rubics_cube[1][9];
+           y=rubics_cube[3][3];
+       }
+       
+       if(rubics_cube[3][3]==rubics_cube[3][5]){
+           x=rubics_cube[5][7];
+           y=rubics_cube[1][9];
+       }
+       
+      if((x==rubics_cube[1][5] && y==rubics_cube[5][5]) || (x==rubics_cube[5][5] && y==rubics_cube[1][5])){
+         to_fix_with_righty_alg();
+      }else{
+          righty_alg(1);
+          bottom_layer_corners();
+      }
+
+      spin_cw(1);
+   }
+   
 
 }
 
@@ -1076,7 +1092,7 @@ void cube::to_move_edge_left(){
    righty_alg(1);
 }
 
-void cube::second_layer_edges(){
+void cube::second_layer_edges(){//To solve 2nd layer edges
 int x,y,t=8;
   while(t--){
 
@@ -1185,12 +1201,16 @@ int cube::check_form_in_step5(){
    vector<int> v(4);
    vector<vector<pair<int,int>>> positions;
    
+   //storing corners of top layer in U shape
    positions.push_back({make_pair(0,1),make_pair(2,1),make_pair(4,3)});
    positions.push_back({make_pair(0,7),make_pair(2,3),make_pair(1,1)});
    positions.push_back({make_pair(0,9),make_pair(5,1),make_pair(1,3)});
    positions.push_back({make_pair(0,3),make_pair(4,1),make_pair(5,3)});
 
    while(1){
+      for(int i=0;i<4;i++)
+      v[i]=0;
+
       count=0;
       for(int i=0;i<4;i++){
          pair<int,int> p1,p2,p3;
@@ -1248,7 +1268,7 @@ int cube::check_form_in_step5(){
    
 }
 
-void cube::position_toplayer_corners(){
+void cube::position_toplayer_corners(){ 
    int x=check_form_in_step5();
    while(x!=1){
       sqnce_in_step5();
@@ -1256,17 +1276,21 @@ void cube::position_toplayer_corners(){
    }
 }
 
-void cube::twisting_top_layer_corners(){
+void cube::twisting_top_layer_corners(){//To solve top layer corners
    rotate_upside_down();
-  int t=8;
+  int t=4;
   while(t--){
-    while((rubics_cube[5][7]==rubics_cube[3][5]) || (rubics_cube[1][9]==rubics_cube[3][5])){
+    while(rubics_cube[3][3]!=rubics_cube[3][5]){
       righty_alg(1);
     }
-   
+
     bottom_face_cw(1);
   }
   rotate_upside_down();
+
+  while(rubics_cube[1][1]!=rubics_cube[1][5]){
+   bottom_2layers_acw(1);
+  }
 }
 
 void cube::sqnce_for_top_layer_edges(){
@@ -1297,14 +1321,16 @@ int cube::check_form_in_lst_step(){
    return 1;
 
    if(count==1){
-      for(int i=0;i<j;i++){
-         spin_cw(1);
-      }
+      while(rubics_cube[1][2]!=rubics_cube[1][5])
+      spin_cw(1);
+      
       return 0;
    }
+
+   return 0;
 }
 
-void cube::top_layer_edges(){
+void cube::top_layer_edges(){ //To solve top layer edges
    int x=check_form_in_lst_step();
    if(x==1){
       return;
@@ -1321,32 +1347,97 @@ void cube::top_layer_edges(){
       sqnce_for_top_layer_edges();
 
       int x=check_form_in_lst_step();
-      if(x==0){
-         spin_cw(1);
+
+      while(x!=1){
          sqnce_for_top_layer_edges();
+         x=check_form_in_lst_step();
       }
+      
    }
 }
+
 
 int main(){
     
   cube c;
+
+  cout << "Give Initial State of Cube:\n";
   c.take_input();
   
-  //for last layer corners
-  //c.bottom_layer_corners();
+  c.bottom_face_plus();
+
+  cout << "\nCube After Solving Bottom Layer Edges:\n";
+  c.print_cube();
+  cout << "\n";
+
+  c.bottom_layer_corners();
+
+  cout << "Cube After Solving Bottom Layer Corners:\n";
+  c.print_cube();
+  cout << "\n";
    
-   c.locate_origin();
+  c.second_layer_edges();
 
-  /*c.second_layer_edges();
+  cout << "Cube After Solving Middle Layer Edges:\n";
   c.print_cube();
+  cout << "\n";
+  
   c.top_layer_plus_step4();
-  c.print_cube();
-  c.position_toplayer_corners();
-  c.print_cube();
-  c.twisting_top_layer_corners();
-  c.print_cube();
-  c.top_layer_edges();*/
-  c.print_cube();
 
+  cout << "Cube After Making PLUS in Top face:\n";
+  c.print_cube();
+  cout << "\n";
+
+  c.position_toplayer_corners();
+
+  cout << "Cube After Positioning Top Layer Corners:\n";
+  c.print_cube();
+  cout << "\n";
+
+  c.twisting_top_layer_corners();
+
+  cout << "Cube After Solving Top Layer corners:\n";
+  c.print_cube();
+  cout << "\n";
+
+  c.top_layer_edges();
+
+  cout << "Cube After Solving Top Layer Edges:\n";
+  c.print_cube();
+  cout << "\n";
+
+  cout << "Solved Cube:\n";
+  c.print_cube();
+  cout << "\n";
 }
+
+/*
+give top face of cube:
+r g r w y o g y o
+give front face of cube:
+y b b w r w b o y
+give left face of cube:
+y g o g b b w y w
+give down face of cube:
+r g b o w w g b w
+give back face of cube:
+g r g y o y b r o
+give right face of cube:
+y b w r g r r o o
+*/
+
+/*
+top face
+y g b y y w g b o
+front face
+r r b w r y g o g
+left face
+o g y y b o w b w
+down face
+o g r w w o b y w
+back face
+y r g w o o b b r
+right face
+y g r r g r w b o
+
+*/
